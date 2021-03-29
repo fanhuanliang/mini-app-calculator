04a995be373d5a0d2b52668f3c0560b049c4ab9c
Huanliang Fan <fanhuanliang@gmail.com>
2021-03-28 17:01:11 -0700

    Initially set up basic router.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,46 @@
 import React from 'react';
+import  {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link 
+} from 'react-router-dom';
 
-const App = () => {
-  return <h1>INITIAL</h1>
+const Home = () => <h2>Home</h2>
+const About = () => <h2>About</h2>
+const Dashboard = () => <h2>Dashboard</h2>
+
+function App() {
+  return (
+  <Router>
+    <div>
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='about'>About</Link>
+        </li>
+        <li>
+          <Link to='dashboard'>Dashboard</Link>
+        </li>
+      </ul>
+
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/about'>
+          <About />
+        </Route>
+        <Route path='/dashboard'>
+          <Dashboard />
+        </Route>
+      </Switch>
+    </div>
+
+  </Router>
+  )
 }
 
 export default App;
